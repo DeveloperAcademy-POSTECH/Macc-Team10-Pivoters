c74990ea691825b828cfe55e9414b34394045541
sozohoy <sozohoy@gmail.com>
2024-06-20 17:15:43 +0900

    chore: Tuist 버전 변경에 따른 파라미터명 수정, 생성자 수정(#131)

--- Tuist/ProjectDescriptionHelpers/Target+extensions.swift
+++ Tuist/ProjectDescriptionHelpers/Target+extensions.swift
@@ -8,32 +8,32 @@
 import ProjectDescription
 
 extension Target {
-  public static func make(
-      name: String,
-      platform: Platform = .iOS,
-      product: Product,
-      bundleId: String,
-      deploymentTarget: DeploymentTarget = .iOS(targetVersion: "17.0", devices: [.iphone]),
-      infoPlist: InfoPlist? = .default,
-      sources: SourceFilesList,
-      resources: ResourceFileElements? = nil,
-      dependencies: [TargetDependency] = [],
-      settings: Settings? = nil,
-      scripts: [TargetScript] = []
-    ) -> Target {
-      return Target(
-        name: name,
-        platform: platform,
-        product: product,
-        bundleId: bundleId,
-        deploymentTarget: deploymentTarget,
-        infoPlist: infoPlist,
-        sources: sources,
-        resources: resources,
-        scripts: scripts,
-        dependencies: dependencies,
-        settings: settings
-      )
-    }
+//  public static func make(
+//      name: String,
+//      platform: Platform = .iOS,
+//      product: Product,
+//      bundleId: String,
+//      deploymentTarget: DeploymentTarget = .iOS(targetVersion: "17.0", devices: [.iphone]),
+//      infoPlist: InfoPlist? = .default,
+//      sources: SourceFilesList,
+//      resources: ResourceFileElements? = nil,
+//      dependencies: [TargetDependency] = [],
+//      settings: Settings? = nil,
+//      scripts: [TargetScript] = []
+//    ) -> Target {
+//      return Target(
+//        name: name,
+//        platform: platform,
+//        product: product,
+//        bundleId: bundleId,
+//        deploymentTarget: deploymentTarget,
+//        infoPlist: infoPlist,
+//        sources: sources,
+//        resources: resources,
+//        scripts: scripts,
+//        dependencies: dependencies,
+//        settings: settings
+//      )
+//    }
 }
 
